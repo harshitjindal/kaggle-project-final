094d1b682b284f5d5b991984cafe3aa4c70ec7b9
amitesh1234 <42119695+amitesh1234@users.noreply.github.com>
2020-09-21 18:29:52 +0530

    Initial Commit

---
+++ final.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 02:16:03 2020
+
+@author: PREDATOR
+"""
+
+import pandas as pd
+import numpy as np
+import re 
+import random
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
+from sklearn.metrics import accuracy_score,classification_report
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
+from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from matplotlib import pyplot as plt
+from sklearn.ensemble import RandomForestClassifier as cl
+
+train_data = pd.read_csv("D:\Local_Directories\Desktop\data/train.csv")
+test_data=pd.read_csv("D:\Local_Directories\Desktop\data/test.csv")
+
+features = ["Weekday", "NumItems", "Return", 'ACCESSORIES',
+       'AUTOMOTIVE', 'BAKERY', 'BATH AND SHOWER', 'BEAUTY', 'BEDDING',
+       'BOOKS AND MAGAZINES', 'BOYS WEAR', 'BRAS & SHAPEWEAR',
+       'CAMERAS AND SUPPLIES', 'CANDY, TOBACCO, COOKIES', 'CELEBRATION',
+       'COMM BREAD', 'CONCEPT STORES', 'COOK AND DINE', 'DAIRY', 'DSD GROCERY',
+       'ELECTRONICS', 'FABRICS AND CRAFTS', 'FINANCIAL SERVICES',
+       'FROZEN FOODS', 'FURNITURE', 'GIRLS WEAR, 4-6X  AND 7-14',
+       'GROCERY DRY GOODS', 'HARDWARE', 'HOME DECOR',
+       'HOME MANAGEMENT', 'HORTICULTURE AND ACCESS',
+       'HOUSEHOLD CHEMICALS/SUPP', 'HOUSEHOLD PAPER GOODS',
+       'IMPULSE MERCHANDISE', 'INFANT APPAREL', 'INFANT CONSUMABLE HARDLINES',
+       'JEWELRY AND SUNGLASSES', 'LADIES SOCKS', 'LADIESWEAR',
+       'LARGE HOUSEHOLD GOODS', 'LAWN AND GARDEN', 'LIQUOR,WINE,BEER',
+       'MEAT - FRESH & FROZEN', 'MEDIA AND GAMING',  'MENSWEAR',
+       'OFFICE SUPPLIES', 'OPTICAL - FRAMES', 'OPTICAL - LENSES',
+       'OTHER DEPARTMENTS', 'PAINT AND ACCESSORIES', 'PERSONAL CARE',
+       'PETS AND SUPPLIES', 'PHARMACY OTC', 'PHARMACY RX',
+       'PLAYERS AND ELECTRONICS', 'PLUS AND MATERNITY', 'PRE PACKED DELI',
+       'PRODUCE', 'SEAFOOD', 'SEASONAL', 'SERVICE DELI', 'SHEER HOSIERY',
+       'SHOES', 'SLEEPWEAR/FOUNDATIONS', 'SPORTING GOODS',
+       'SWIMWEAR/OUTERWEAR', 'TOYS', 'WIRELESS']
+
+data=train_data
+dummies = pd.get_dummies(data.DepartmentDescription)
+data[dummies.columns] = dummies 
+data_dummies = data.iloc[:,7:]
+data_dummies = data_dummies.apply(lambda x: x*data["ScanCount"])
+data_dummies = data_dummies.replace(-0,0)
+
+data.loc[data.ScanCount < 0, 'Return'] = 1
+data.loc[data.Return != 1, 'Return'] = 0
+data = data[["TripType", "VisitNumber", "Weekday", "ScanCount", "Return"]]
+data = data.rename(columns={"ScanCount":"NumItems"})
+data = pd.concat([data, data_dummies], axis=1)
+grouped = data.groupby("VisitNumber")
+
+grouped = grouped.agg({'Weekday': np.max, "TripType": np.max, 'NumItems': np.sum, 'Return': np.max, 
+              '1-HR PHOTO': np.sum, 'ACCESSORIES': np.sum,
+       'AUTOMOTIVE': np.sum, 'BAKERY': np.sum, 'BATH AND SHOWER': np.sum, 'BEAUTY': np.sum, 'BEDDING': np.sum,
+       'BOOKS AND MAGAZINES': np.sum, 'BOYS WEAR': np.sum, 'BRAS & SHAPEWEAR': np.sum,
+       'CAMERAS AND SUPPLIES': np.sum, 'CANDY, TOBACCO, COOKIES': np.sum, 'CELEBRATION': np.sum,
+       'COMM BREAD': np.sum, 'CONCEPT STORES': np.sum, 'COOK AND DINE': np.sum, 'DAIRY': np.sum, 'DSD GROCERY': np.sum,
+       'ELECTRONICS': np.sum, 'FABRICS AND CRAFTS': np.sum, 'FINANCIAL SERVICES': np.sum,
+       'FROZEN FOODS': np.sum, 'FURNITURE': np.sum, 'GIRLS WEAR, 4-6X  AND 7-14': np.sum,
+       'GROCERY DRY GOODS': np.sum, 'HARDWARE': np.sum, 'HEALTH AND BEAUTY AIDS': np.sum, 'HOME DECOR': np.sum,
+       'HOME MANAGEMENT': np.sum, 'HORTICULTURE AND ACCESS': np.sum,
+       'HOUSEHOLD CHEMICALS/SUPP': np.sum, 'HOUSEHOLD PAPER GOODS': np.sum,
+       'IMPULSE MERCHANDISE': np.sum, 'INFANT APPAREL': np.sum, 'INFANT CONSUMABLE HARDLINES': np.sum,
+       'JEWELRY AND SUNGLASSES': np.sum, 'LADIES SOCKS': np.sum, 'LADIESWEAR': np.sum,
+       'LARGE HOUSEHOLD GOODS': np.sum, 'LAWN AND GARDEN': np.sum, 'LIQUOR,WINE,BEER': np.sum,
+       'MEAT - FRESH & FROZEN': np.sum, 'MEDIA AND GAMING': np.sum,'MENSWEAR': np.sum,
+       'OFFICE SUPPLIES': np.sum, 'OPTICAL - FRAMES': np.sum, 'OPTICAL - LENSES': np.sum,
+       'OTHER DEPARTMENTS': np.sum, 'PAINT AND ACCESSORIES': np.sum, 'PERSONAL CARE': np.sum,
+       'PETS AND SUPPLIES': np.sum, 'PHARMACY OTC': np.sum, 'PHARMACY RX': np.sum,
+       'PLAYERS AND ELECTRONICS': np.sum, 'PLUS AND MATERNITY': np.sum, 'PRE PACKED DELI': np.sum,
+       'PRODUCE': np.sum, 'SEAFOOD': np.sum, 'SEASONAL': np.sum, 'SERVICE DELI': np.sum, 'SHEER HOSIERY': np.sum,
+       'SHOES': np.sum, 'SLEEPWEAR/FOUNDATIONS': np.sum, 'SPORTING GOODS': np.sum,
+       'SWIMWEAR/OUTERWEAR': np.sum, 'TOYS': np.sum, 'WIRELESS': np.sum})
+
+data = grouped[["TripType", "Weekday", "NumItems", "Return",'1-HR PHOTO', 'ACCESSORIES',
+           'AUTOMOTIVE', 'BAKERY', 'BATH AND SHOWER', 'BEAUTY', 'BEDDING',
+           'BOOKS AND MAGAZINES', 'BOYS WEAR', 'BRAS & SHAPEWEAR',
+           'CAMERAS AND SUPPLIES', 'CANDY, TOBACCO, COOKIES', 'CELEBRATION',
+           'COMM BREAD', 'CONCEPT STORES', 'COOK AND DINE', 'DAIRY', 'DSD GROCERY',
+           'ELECTRONICS', 'FABRICS AND CRAFTS', 'FINANCIAL SERVICES',
+           'FROZEN FOODS', 'FURNITURE', 'GIRLS WEAR, 4-6X  AND 7-14',
+           'GROCERY DRY GOODS', 'HARDWARE', 'HEALTH AND BEAUTY AIDS', 'HOME DECOR',
+           'HOME MANAGEMENT', 'HORTICULTURE AND ACCESS',
+           'HOUSEHOLD CHEMICALS/SUPP', 'HOUSEHOLD PAPER GOODS',
+           'IMPULSE MERCHANDISE', 'INFANT APPAREL', 'INFANT CONSUMABLE HARDLINES',
+           'JEWELRY AND SUNGLASSES', 'LADIES SOCKS', 'LADIESWEAR',
+           'LARGE HOUSEHOLD GOODS', 'LAWN AND GARDEN', 'LIQUOR,WINE,BEER',
+           'MEAT - FRESH & FROZEN', 'MEDIA AND GAMING', 'MENSWEAR',
+           'OFFICE SUPPLIES', 'OPTICAL - FRAMES', 'OPTICAL - LENSES',
+           'OTHER DEPARTMENTS', 'PAINT AND ACCESSORIES', 'PERSONAL CARE',
+           'PETS AND SUPPLIES', 'PHARMACY OTC', 'PHARMACY RX',
+           'PLAYERS AND ELECTRONICS', 'PLUS AND MATERNITY', 'PRE PACKED DELI',
+           'PRODUCE', 'SEAFOOD', 'SEASONAL', 'SERVICE DELI', 'SHEER HOSIERY',
+           'SHOES', 'SLEEPWEAR/FOUNDATIONS', 'SPORTING GOODS',
+           'SWIMWEAR/OUTERWEAR', 'TOYS', 'WIRELESS']]
+
+data=data.dropna()
+
+data=data.sample(frac=1)
+
+
+def transform(day,no):
+    global data
+    data=data.replace(day,no)
+    return;
+transform("Monday", 1)
+transform("Tuesday", 2)
+transform("Wednesday", 3)
+transform("Thursday", 4)
+transform("Friday", 5)
+transform("Saturday", 6)
+transform("Sunday", 7)
+
+data=data.replace("MENS WEAR","MENSWEAR")
+
+train, test= train_test_split(data, test_size=0.3)
+
+data.T.apply(lambda x: x.nunique(),axis=1)
+
+data.TripType = data.TripType.astype(float)
+data.Weekday = data.Weekday.astype(float)
+ ###########logistic regression
+lr = LogisticRegression(C= 0.1, penalty='l2', solver = 'liblinear')
+lr.fit(X = x_train, y = y)
+
+predictions = lr.predict(x_train)
+accuracy_score(y,predictions )
+######decision tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(np.asarray(train[features]), 
+                    np.asarray(train.TripType))
+predictions = clf.predict(np.asarray(train[features]))
+accuracy_score(train.TripType,predictions)
+
+predictions1 = clf.predict(np.asarray(test[features]))
+accuracy_score(test.TripType,predictions1)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder
+from sklearn.compose import ColumnTransformer
+#for the 3 countries
+x=np.asarray(data[features])
+
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x)
+y=np.asarray(data.TripType).astype(int).reshape(-1,1)
+ct1=ColumnTransformer([('one_hotencoder',OneHotEncoder(categories='auto'),[0])],remainder='passthrough')
+y_train=ct1.fit_transform(y)
+y_train=y_train.todense()
+
+#initialising the layer
+classifier = Sequential()
+classifier.add(Dense(output_dim=32, init = 'uniform', activation = 'relu', input_dim=68))
+classifier.add(Dense(output_dim=64, init = 'uniform', activation = 'relu'))
+classifier.add(Dense(output_dim=38, init = 'uniform', activation = 'relu'))
+classifier.add(Dense(output_dim=38, init = 'uniform', activation = 'softmax'))
+classifier.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+classifier.fit(x_train,y_train, batch_size=32, epochs=100,verbose=1)
+
+
+
+
+
+clf1 = RandomForestClassifier(n_estimators=200, max_depth=35)
+clf1.fit(np.asarray(data[features]), 
+                    np.asarray(data.TripType))
+predictions = clf1.predict(np.asarray(train[features]))
+accuracy_score(train.TripType,predictions)
+
+predictions1 = clf1.predict(np.asarray(test[features]))
+accuracy_score(test.TripType,predictions1)
+
+
+
+
+
+
+
+
+
+
+data1=test_data
+dummies1 = pd.get_dummies(data1.DepartmentDescription)
+data1[dummies1.columns] = dummies1 
+data_dummies1 = data1.iloc[:,6:]
+data_dummies1 = data_dummies1.apply(lambda x: x*data1["ScanCount"])
+data_dummies1 = data_dummies1.replace(-0,0)
+
+data1.loc[data1.ScanCount < 0, 'Return'] = 1
+data1.loc[data1.Return != 1, 'Return'] = 0
+data1 = data1[["VisitNumber", "Weekday", "ScanCount", "Return"]]
+data1 = data1.rename(columns={"ScanCount":"NumItems"})
+data1 = pd.concat([data1, data_dummies1], axis=1)
+grouped1 = data1.groupby("VisitNumber")
+
+
+grouped1 = grouped1.agg({'Weekday': np.max, 'NumItems': np.sum, 'Return': np.max, 
+              '1-HR PHOTO': np.sum, 'ACCESSORIES': np.sum,
+       'AUTOMOTIVE': np.sum, 'BAKERY': np.sum, 'BATH AND SHOWER': np.sum, 'BEAUTY': np.sum, 'BEDDING': np.sum,
+       'BOOKS AND MAGAZINES': np.sum, 'BOYS WEAR': np.sum, 'BRAS & SHAPEWEAR': np.sum,
+       'CAMERAS AND SUPPLIES': np.sum, 'CANDY, TOBACCO, COOKIES': np.sum, 'CELEBRATION': np.sum,
+       'COMM BREAD': np.sum, 'CONCEPT STORES': np.sum, 'COOK AND DINE': np.sum, 'DAIRY': np.sum, 'DSD GROCERY': np.sum,
+       'ELECTRONICS': np.sum, 'FABRICS AND CRAFTS': np.sum, 'FINANCIAL SERVICES': np.sum,
+       'FROZEN FOODS': np.sum, 'FURNITURE': np.sum, 'GIRLS WEAR, 4-6X  AND 7-14': np.sum,
+       'GROCERY DRY GOODS': np.sum, 'HARDWARE': np.sum, 'HEALTH AND BEAUTY AIDS': np.sum, 'HOME DECOR': np.sum,
+       'HOME MANAGEMENT': np.sum, 'HORTICULTURE AND ACCESS': np.sum,
+       'HOUSEHOLD CHEMICALS/SUPP': np.sum, 'HOUSEHOLD PAPER GOODS': np.sum,
+       'IMPULSE MERCHANDISE': np.sum, 'INFANT APPAREL': np.sum, 'INFANT CONSUMABLE HARDLINES': np.sum,
+       'JEWELRY AND SUNGLASSES': np.sum, 'LADIES SOCKS': np.sum, 'LADIESWEAR': np.sum,
+       'LARGE HOUSEHOLD GOODS': np.sum, 'LAWN AND GARDEN': np.sum, 'LIQUOR,WINE,BEER': np.sum,
+       'MEAT - FRESH & FROZEN': np.sum, 'MEDIA AND GAMING': np.sum,'MENSWEAR': np.sum,
+       'OFFICE SUPPLIES': np.sum, 'OPTICAL - FRAMES': np.sum, 'OPTICAL - LENSES': np.sum,
+       'OTHER DEPARTMENTS': np.sum, 'PAINT AND ACCESSORIES': np.sum, 'PERSONAL CARE': np.sum,
+       'PETS AND SUPPLIES': np.sum, 'PHARMACY OTC': np.sum, 'PHARMACY RX': np.sum,
+       'PLAYERS AND ELECTRONICS': np.sum, 'PLUS AND MATERNITY': np.sum, 'PRE PACKED DELI': np.sum,
+       'PRODUCE': np.sum, 'SEAFOOD': np.sum, 'SEASONAL': np.sum, 'SERVICE DELI': np.sum, 'SHEER HOSIERY': np.sum,
+       'SHOES': np.sum, 'SLEEPWEAR/FOUNDATIONS': np.sum, 'SPORTING GOODS': np.sum,
+       'SWIMWEAR/OUTERWEAR': np.sum, 'TOYS': np.sum, 'WIRELESS': np.sum})
+
+data = grouped[["TripType", "Weekday", "NumItems", "Return",'1-HR PHOTO', 'ACCESSORIES',
+           'AUTOMOTIVE', 'BAKERY', 'BATH AND SHOWER', 'BEAUTY', 'BEDDING',
+           'BOOKS AND MAGAZINES', 'BOYS WEAR', 'BRAS & SHAPEWEAR',
+           'CAMERAS AND SUPPLIES', 'CANDY, TOBACCO, COOKIES', 'CELEBRATION',
+           'COMM BREAD', 'CONCEPT STORES', 'COOK AND DINE', 'DAIRY', 'DSD GROCERY',
+           'ELECTRONICS', 'FABRICS AND CRAFTS', 'FINANCIAL SERVICES',
+           'FROZEN FOODS', 'FURNITURE', 'GIRLS WEAR, 4-6X  AND 7-14',
+           'GROCERY DRY GOODS', 'HARDWARE', 'HEALTH AND BEAUTY AIDS', 'HOME DECOR',
+           'HOME MANAGEMENT', 'HORTICULTURE AND ACCESS',
+           'HOUSEHOLD CHEMICALS/SUPP', 'HOUSEHOLD PAPER GOODS',
+           'IMPULSE MERCHANDISE', 'INFANT APPAREL', 'INFANT CONSUMABLE HARDLINES',
+           'JEWELRY AND SUNGLASSES', 'LADIES SOCKS', 'LADIESWEAR',
+           'LARGE HOUSEHOLD GOODS', 'LAWN AND GARDEN', 'LIQUOR,WINE,BEER',
+           'MEAT - FRESH & FROZEN', 'MEDIA AND GAMING', 'MENSWEAR',
+           'OFFICE SUPPLIES', 'OPTICAL - FRAMES', 'OPTICAL - LENSES',
+           'OTHER DEPARTMENTS', 'PAINT AND ACCESSORIES', 'PERSONAL CARE',
+           'PETS AND SUPPLIES', 'PHARMACY OTC', 'PHARMACY RX',
+           'PLAYERS AND ELECTRONICS', 'PLUS AND MATERNITY', 'PRE PACKED DELI',
+           'PRODUCE', 'SEAFOOD', 'SEASONAL', 'SERVICE DELI', 'SHEER HOSIERY',
+           'SHOES', 'SLEEPWEAR/FOUNDATIONS', 'SPORTING GOODS',
+           'SWIMWEAR/OUTERWEAR', 'TOYS', 'WIRELESS']]
+
+data=data.dropna()
+
+data=data.sample(frac=1)
+
+
+def transform(day,no):
+    global data
+    data=data.replace(day,no)
+    return;
+transform("Monday", 1)
+transform("Tuesday", 2)
+transform("Wednesday", 3)
+transform("Thursday", 4)
+transform("Friday", 5)
+transform("Saturday", 6)
+transform("Sunday", 7)
+
+data=data.replace("MENS WEAR","MENSWEAR")
